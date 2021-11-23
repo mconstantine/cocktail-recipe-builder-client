c5c9876175176e678b4ba37aafa9084895588c80
mconstantine <mauro.constantinescu@gmail.com>
2021-11-23 17:37:32 +0100

    Added automatic theming.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './components/Header'
 import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 export function App() {
-  const theme = responsiveFontSizes(
-    createTheme({
-      palette: {
-        mode: 'dark',
-      },
-    }),
+  const shouldBeDark =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  const makeTheme = (mode: 'light' | 'dark') =>
+    responsiveFontSizes(createTheme({ palette: { mode } }))
+
+  useEffect(() => {
+    const onThemeChange = (e: MediaQueryListEvent) => {
+      setTheme(makeTheme(e.matches ? 'dark' : 'light'))
+    }
+
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', onThemeChange)
+
+    return () => {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', onThemeChange)
+    }
+  }, [])
+
+  const [theme, setTheme] = useState(
+    responsiveFontSizes(
+      createTheme({
+        palette: {
+          mode: shouldBeDark ? 'dark' : 'light',
+        },
+      }),
+    ),
   )
 
   return (
