654077f91f5843471675498ecafe960929f15f45
mconstantine <mauro.constantinescu@gmail.com>
2021-12-10 19:55:00 +0100

    Better "edit ingredients" experience.

--- src/components/CocktailForm/IngredientsForm.tsx
+++ src/components/CocktailForm/IngredientsForm.tsx
@@ -30,17 +30,18 @@ import {
 import { ingredientRangesToString } from '../../utils/ingredientRangesToString'
 import { NumberField } from '../NumberField'
 import {
-  cancelAction,
+  close,
   foldState,
   importAction,
   reducer,
   saveAction,
-  startAction,
+  openAction,
   stateToCocktailIngredient,
   updateAmountAction,
   updateIngredientAction,
   updateUnitAction,
   validateState,
+  State,
 } from './IngredientsFormState'
 
 interface Props {
@@ -56,7 +57,8 @@ export function IngredientsForm(props: Props) {
     state,
     foldState({
       READY: constFalse,
-      ADDING: flow(validateState, option.isSome),
+      ADDING: state => pipe(validateState(state), option.isSome),
+      EDITING: state => pipe(validateState(state), option.isSome),
     }),
   )
 
@@ -83,16 +85,31 @@ export function IngredientsForm(props: Props) {
       state,
       foldState({
         READY: constVoid,
-        ADDING: flow(
+        ADDING: state =>
+          pipe(
+            validateState(state),
+            option.fold(constVoid, state => {
+              props.onChange([
+                ...props.ingredients,
+                stateToCocktailIngredient(state),
+              ])
+              dispatch(close())
+            }),
+          ),
+        EDITING: flow(
           validateState,
           option.fold(constVoid, state => {
-            props.onChange([
-              ...props.ingredients.filter(
-                i => i.ingredient.id !== state.ingredient.id,
-              ),
-              stateToCocktailIngredient(state),
-            ])
-            dispatch(cancelAction())
+            props.onChange(
+              props.ingredients.map((ingredient, index) => {
+                if (index === state.originalIngredientIndex) {
+                  return stateToCocktailIngredient(state)
+                } else {
+                  return ingredient
+                }
+              }),
+            )
+
+            dispatch(close())
           }),
         ),
       }),
@@ -109,12 +126,82 @@ export function IngredientsForm(props: Props) {
     )
   }
 
+  const renderForm = ({
+    ingredient,
+    amount,
+    unit,
+  }: Omit<Extract<State, { type: 'ADDING' }>, 'type'>) => (
+    <>
+      <Autocomplete
+        options={pipe(
+          ingredients,
+          query.map(({ data }) => data),
+          query.getOrElse(() => [] as IngredientsOutput['data']),
+        )}
+        getOptionLabel={({ name }) => name}
+        renderInput={params => (
+          <TextField {...params} label="Ingredient" required />
+        )}
+        value={pipe(ingredient, option.toNullable)}
+        onInputChange={(_, query) => updateIngredientsQuery(query)}
+        loading={isLoadingIngredients || query.isLoading(ingredients)}
+        onChange={(_, value) =>
+          value && dispatch(updateIngredientAction(value))
+        }
+        disabled={props.disabled}
+        renderOption={(props, option) => (
+          <ListItem {...props}>
+            <ListItemText
+              primary={`${option.name}`}
+              secondary={ingredientRangesToString(option)}
+            />
+          </ListItem>
+        )}
+      />
+      <NumberField
+        label="Amount"
+        value={pipe(
+          amount,
+          option.getOrElse(() => 0),
+        )}
+        onChange={flow(updateAmountAction, dispatch)}
+        min={0}
+        disabled={props.disabled}
+      />
+      <Autocomplete
+        options={pipe(
+          units,
+          query.getOrElse(() => [] as IngredientUnit[]),
+        )}
+        getOptionLabel={({ unit }) => unit}
+        renderInput={params => <TextField {...params} label="Unit" required />}
+        value={pipe(unit, option.toNullable)}
+        loading={query.isLoading(units)}
+        onChange={(_, value) => value && dispatch(updateUnitAction(value))}
+        disabled={props.disabled}
+      />
+      <Stack direction="row" spacing={2}>
+        <Button
+          variant="outlined"
+          startIcon={<Save />}
+          onClick={onAddIngredientClick}
+          disabled={props.disabled || !isStateValid}
+        >
+          Save
+        </Button>
+        <Button color="inherit" onClick={() => dispatch(close())}>
+          Close
+        </Button>
+      </Stack>
+    </>
+  )
+
   return (
     <Stack spacing={4}>
       <Typography variant="h6">Ingredients:</Typography>
       {props.ingredients.length ? (
         <List>
-          {props.ingredients.map(ingredient => (
+          {props.ingredients.map((ingredient, index) => (
             <ListItem
               key={ingredient.ingredient.id}
               secondaryAction={
@@ -129,7 +216,7 @@ export function IngredientsForm(props: Props) {
               }
             >
               <ListItemButton
-                onClick={() => dispatch(importAction(ingredient))}
+                onClick={() => dispatch(importAction(ingredient, index))}
                 disabled={props.disabled}
               >
                 <ListItemText
@@ -145,78 +232,8 @@ export function IngredientsForm(props: Props) {
         state,
         foldState({
           READY: constNull,
-          ADDING: ({ ingredient, amount, unit }) => (
-            <>
-              <Autocomplete
-                options={pipe(
-                  ingredients,
-                  query.map(({ data }) => data),
-                  query.getOrElse(() => [] as IngredientsOutput['data']),
-                )}
-                getOptionLabel={({ name }) => name}
-                renderInput={params => (
-                  <TextField {...params} label="Ingredient" required />
-                )}
-                value={pipe(ingredient, option.toNullable)}
-                onInputChange={(_, query) => updateIngredientsQuery(query)}
-                loading={isLoadingIngredients || query.isLoading(ingredients)}
-                onChange={(_, value) =>
-                  value && dispatch(updateIngredientAction(value))
-                }
-                disabled={props.disabled}
-                renderOption={(props, option) => (
-                  <ListItem {...props}>
-                    <ListItemText
-                      primary={`${option.name}`}
-                      secondary={ingredientRangesToString(option)}
-                    />
-                  </ListItem>
-                )}
-              />
-              <NumberField
-                label="Amount"
-                value={pipe(
-                  amount,
-                  option.getOrElse(() => 0),
-                )}
-                onChange={flow(updateAmountAction, dispatch)}
-                min={0}
-                disabled={props.disabled}
-              />
-              <Autocomplete
-                options={pipe(
-                  units,
-                  query.getOrElse(() => [] as IngredientUnit[]),
-                )}
-                getOptionLabel={({ unit }) => unit}
-                renderInput={params => (
-                  <TextField {...params} label="Unit" required />
-                )}
-                value={pipe(unit, option.toNullable)}
-                loading={query.isLoading(units)}
-                onChange={(_, value) =>
-                  value && dispatch(updateUnitAction(value))
-                }
-                disabled={props.disabled}
-              />
-              <Stack direction="row" spacing={2}>
-                <Button
-                  variant="outlined"
-                  startIcon={<Save />}
-                  onClick={onAddIngredientClick}
-                  disabled={props.disabled || !isStateValid}
-                >
-                  Save
-                </Button>
-                <Button
-                  color="inherit"
-                  onClick={() => dispatch(cancelAction())}
-                >
-                  Close
-                </Button>
-              </Stack>
-            </>
-          ),
+          ADDING: renderForm,
+          EDITING: renderForm,
         }),
       )}
       <Box>
@@ -227,13 +244,14 @@ export function IngredientsForm(props: Props) {
               <Button
                 variant="outlined"
                 startIcon={<Add />}
-                onClick={() => dispatch(startAction())}
+                onClick={() => dispatch(openAction())}
                 disabled={props.disabled}
               >
                 Add
               </Button>
             ),
             ADDING: constNull,
+            EDITING: constNull,
           }),
         )}
       </Box>
--- src/components/CocktailForm/IngredientsFormState.tsx
+++ src/components/CocktailForm/IngredientsFormState.tsx
@@ -24,12 +24,6 @@ interface AddingState {
   unit: Option<IngredientUnit>
 }
 
-interface ValidState {
-  ingredient: Ingredient
-  amount: number
-  unit: IngredientUnit
-}
-
 function addingState(data: Omit<AddingState, 'type'>): AddingState {
   return {
     type: 'ADDING',
@@ -37,15 +31,52 @@ function addingState(data: Omit<AddingState, 'type'>): AddingState {
   }
 }
 
-export function validateState(state: AddingState): Option<ValidState> {
+interface EditingState {
+  type: 'EDITING'
+  originalIngredientIndex: number
+  ingredient: Option<Ingredient>
+  amount: Option<number>
+  unit: Option<IngredientUnit>
+}
+
+function editingState(data: Omit<EditingState, 'type'>): EditingState {
+  return {
+    type: 'EDITING',
+    ...data,
+  }
+}
+
+interface ValidAddingState {
+  ingredient: Ingredient
+  amount: number
+  unit: IngredientUnit
+}
+
+interface ValidEditingState extends ValidAddingState {
+  originalIngredientIndex: number
+}
+
+export function validateState(state: AddingState): Option<ValidAddingState>
+export function validateState(state: EditingState): Option<ValidEditingState>
+export function validateState(
+  state: AddingState | EditingState,
+): Option<ValidAddingState | ValidEditingState> {
   return pipe(
     { ingredient: state.ingredient, amount: state.amount, unit: state.unit },
     sequenceS(option.Apply),
+    option.map(validState =>
+      state.type === 'ADDING'
+        ? validState
+        : {
+            ...validState,
+            originalIngredientIndex: state.originalIngredientIndex,
+          },
+    ),
   )
 }
 
 export function stateToCocktailIngredient(
-  state: ValidState,
+  state: ValidAddingState,
 ): CocktailIngredient {
   const ingredient = state.ingredient
   const amount = state.amount
@@ -58,16 +89,15 @@ export function stateToCocktailIngredient(
   }
 }
 
-function emptyIngredient(): AddingState {
+function emptyIngredient(): Omit<AddingState, 'type'> {
   return {
-    type: 'ADDING',
     ingredient: option.none,
     amount: option.none,
     unit: option.none,
   }
 }
 
-export type State = ReadyState | AddingState
+export type State = ReadyState | AddingState | EditingState
 
 export function foldState<T>(cases: {
   [k in State['type']]: Reader<Extract<State, { type: k }>, T>
@@ -75,12 +105,12 @@ export function foldState<T>(cases: {
   return state => cases[state.type](state as any)
 }
 
-interface StartAction {
-  type: 'START'
+interface OpenAction {
+  type: 'OPEN'
 }
 
-export function startAction(): StartAction {
-  return { type: 'START' }
+export function openAction(): OpenAction {
+  return { type: 'OPEN' }
 }
 
 interface SaveAction {
@@ -91,12 +121,12 @@ export function saveAction(): SaveAction {
   return { type: 'SAVE' }
 }
 
-interface CancelAction {
-  type: 'CANCEL'
+interface CloseAction {
+  type: 'CLOSE'
 }
 
-export function cancelAction(): CancelAction {
-  return { type: 'CANCEL' }
+export function close(): CloseAction {
+  return { type: 'CLOSE' }
 }
 
 interface UpdateIngredientAction {
@@ -131,16 +161,20 @@ export function updateUnitAction(unit: IngredientUnit): UpdateUnitAction {
 interface ImportAction {
   type: 'IMPORT'
   ingredient: CocktailIngredient
+  originalIngredientIndex: number
 }
 
-export function importAction(ingredient: CocktailIngredient): ImportAction {
-  return { type: 'IMPORT', ingredient }
+export function importAction(
+  ingredient: CocktailIngredient,
+  originalIngredientIndex: number,
+): ImportAction {
+  return { type: 'IMPORT', ingredient, originalIngredientIndex }
 }
 
 type Action =
-  | StartAction
+  | OpenAction
   | SaveAction
-  | CancelAction
+  | CloseAction
   | UpdateIngredientAction
   | UpdateAmountAction
   | UpdateUnitAction
@@ -150,15 +184,16 @@ export function reducer(state: State, action: Action): State {
   switch (state.type) {
     case 'READY':
       switch (action.type) {
-        case 'START':
-          return emptyIngredient()
+        case 'OPEN':
+          return addingState(emptyIngredient())
         case 'IMPORT':
-          return addingState({
+          return editingState({
+            originalIngredientIndex: action.originalIngredientIndex,
             ingredient: option.some(action.ingredient.ingredient),
             amount: option.some(action.ingredient.amount),
             unit: option.some(action.ingredient.unit),
           })
-        case 'CANCEL':
+        case 'CLOSE':
         case 'SAVE':
         case 'UPDATE_INGREDIENT':
         case 'UPDATE_AMOUNT':
@@ -167,15 +202,17 @@ export function reducer(state: State, action: Action): State {
       }
     case 'ADDING':
       switch (action.type) {
-        case 'START':
+        case 'OPEN':
           return state
-        case 'CANCEL':
+        case 'CLOSE':
           return readyState()
         case 'SAVE':
           return pipe(
-            state,
-            validateState,
-            option.fold(() => state, emptyIngredient),
+            validateState(state),
+            option.fold(
+              () => state,
+              () => addingState(emptyIngredient()),
+            ),
           )
         case 'UPDATE_INGREDIENT':
           return addingState({
@@ -193,7 +230,46 @@ export function reducer(state: State, action: Action): State {
             unit: option.some(action.unit),
           })
         case 'IMPORT':
-          return addingState({
+          return editingState({
+            originalIngredientIndex: action.originalIngredientIndex,
+            ingredient: option.some(action.ingredient.ingredient),
+            amount: option.some(action.ingredient.amount),
+            unit: option.some(action.ingredient.unit),
+          })
+      }
+    case 'EDITING':
+      switch (action.type) {
+        case 'OPEN':
+          return state
+        case 'CLOSE':
+          return readyState()
+        case 'SAVE':
+          return pipe(
+            state,
+            validateState,
+            option.fold(
+              () => state as State,
+              () => addingState(emptyIngredient()),
+            ),
+          )
+        case 'UPDATE_INGREDIENT':
+          return editingState({
+            ...state,
+            ingredient: option.some(action.ingredient),
+          })
+        case 'UPDATE_AMOUNT':
+          return editingState({
+            ...state,
+            amount: option.some(action.amount),
+          })
+        case 'UPDATE_UNIT':
+          return editingState({
+            ...state,
+            unit: option.some(action.unit),
+          })
+        case 'IMPORT':
+          return editingState({
+            originalIngredientIndex: action.originalIngredientIndex,
             ingredient: option.some(action.ingredient.ingredient),
             amount: option.some(action.ingredient.amount),
             unit: option.some(action.ingredient.unit),
