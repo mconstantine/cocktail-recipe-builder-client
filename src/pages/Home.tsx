eda5238823c14f45d6ea79ad1bd63c3ebd1fdc3c
mconstantine <mauro.constantinescu@gmail.com>
2022-01-26 12:11:22 +0100

    Added rel noreferrer to target blank link.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export function Home() {
       <Typography variant="h1">Cocktail recipe builder</Typography>
       <Typography>
         Welcome to the cocktail recipe builder by{' '}
-        <a href="https://www.mconst.it" target="_blank">
+        <a href="https://www.mconst.it" rel="noreferrer" target="_blank">
           Mauro Constantinescu
         </a>
         . Use the menu up above to start exploring!
